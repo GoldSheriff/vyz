a41578f4bef33ce90f464de62d74f9c536dd4b85
GoldSheriff <dlavyza@gmail.com>
2023-03-13 22:00:51 +0300

    lr2_1

---
+++ lr2.rb
@@ -0,0 +1,14 @@
+def greeting (name, surname, years)
+	puts "Привет, #{name} #{surname}. Вам еще нет 18 лет, но никогда не рано начинать учиться программированию." if years < 18
+	puts "Привет, #{name} #{surname}. Пришло время заняться делом!" if years > 18
+end
+puts "Введите имя:"
+name = gets.chomp
+puts "Введите фамилию:"
+surname = gets.chomp
+puts "Ваш возраст:"
+years = gets.to_i
+greeting(name, surname, years)
+puts "Поменять возраст:"
+years = gets.to_i
+greeting(name, surname, years)
